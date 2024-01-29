f7a3fa7d3a779e0291c4886863ee7ef4c436cb24
sajinadissanayake <125750172+sajinadissanayake@users.noreply.github.com>
2024-01-30 01:09:20 +0530

    Patient Registration backend

---
+++ server/models/patients.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const patientSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+
+    address: String,
+    dob: {
+        type: Date,
+        default: new Date().setHours(0, 0, 0, 0) // Set time to midnight
+    },// Make sure to handle date appropriately
+    
+    pnumber: Number,
+    moh: String,
+    phm: String,
+    phi: String,
+    gnd: String,
+    dsd: String,
+    neighbour: String,
+   
+});
+
+    
+
+
+const patientModel = mongoose.model("patients", patientSchema)
+module.exports = patientModel
